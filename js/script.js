4f7ec6edac80ec3ca1c1c75e48e0b27af21fd8a6
fED <fed_v@hotmail.com>
2015-07-11 17:03:16 -0400

    Added all files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*jslint browser: true*/
+/*global $, jQuery, console*/
+/*jslint indent: 4, maxerr: 50, vars: true, regexp: true, sloppy: true */
+jQuery(document).ready(function ($) {
+
+    'use strict';
+
+
+    /****************************************/
+    /***        Set copyright date       ***/
+    /***************************************/
+
+    //CREATE INSTANCE OF THE DATE OBJECT
+    var date = new Date();
+
+    //EXTRACT FOUR DIGIT YEAR AND ADD TO SPAN
+    document.getElementById('year').innerHTML = date.getFullYear();
+
+});
